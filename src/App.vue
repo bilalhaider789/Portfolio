6bd172fae633b12cea30b6dbc6c57751775398c0
bilalhaider789 <bilalhaider1311@gmail.com>
2024-04-14 17:36:02 +0500

    refact: refactored the skills part

--- src/App.vue
+++ src/App.vue
@@ -15,95 +15,9 @@
           <SpringHeading></SpringHeading>
         </div>
       </section>
+      <Skills></Skills>
     </div>
-    <div class="custom-shape-divider-top-1708193282 bg-[#f8f8f8] mt-[-2px]">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path
-          d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-          class="shape-fill fill-quaternary"></path>
-      </svg>
-    </div>
-    <section class="bg-[#f8f8f8] min-h-[100vh] flex flex-col font-merienda px-4 lg:px-6" >
-      <div class="w-full flex justify-center">
-        <h1 class="responsive-h1 lg:text-[3.5rem]">Skills</h1>
-      </div>
-      <h2 class="responsive-h3 mt-4 font-semibold">FrontEnd</h2>
-      <div class="skills responsive-p1 grid grid-cols-3 lg:grid-cols-4 gap-4 mt-4 lg:mt-6">
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/react.svg" /><span>React.js</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/vue.svg" /><span>Vue.js</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/nextjs.svg" /><span>Next.js</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/flutter.svg" /><span>Flutter</span></div>
-        <div class="flex gap-2 items-center "><img class="h-10 w-10"
-            src="./assets/imgs/skills/state-management.svg" /><span class="white">State Management</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/tailwind.svg" /><span>Tailwind Css</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/material-ui.svg" /><span>Material UI</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/html.svg" /><span>HTML</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/css.svg" /><span>CSS</span></div>
-      </div>
-      <h2 class="responsive-h3 mt-6 font-semibold">BackEnd</h2>
-      <div class="skills responsive-p1 grid grid-cols-3 lg:grid-cols-4 gap-4 mt-4 lg:mt-6">
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/node.svg" /><span>Node</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/nestjs.svg" /><span>Nest</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/sequelize.svg" /><span>Sequelize</span></div>
-        <div class="flex gap-2 items-center "><img class="h-10 w-10"
-            src="./assets/imgs/skills/typeorm.svg" /><span>TypeOrm</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/database.svg" /><span>PostgresSQL</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/firebase.svg" /><span>FireBase</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/mongodb.svg" /><span>MongoDB</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/influxdb.svg" /><span>InfluxDB</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10" src="./assets/imgs/skills/api.svg" /><span>Rest
-            </span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/protobuf.svg" /><span>ProtoBuf</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/grpc.svg" /><span>gRPC</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/flask.svg" /><span>Flask</span></div>
-        <div class="flex gap-2 items-center"><img class="h-6 w-10"
-            src="./assets/imgs/skills/mqtt.svg" /><span>Mqtt</span></div>
-
-      </div>
-
-      <h2 class="responsive-h3 mt-6 font-semibold">Other</h2>
-      <div class="skills responsive-p1 grid grid-cols-3 lg:grid-cols-4 gap-4 mt-4 lg:mt-6">
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/docker.svg" /><span>Docker</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/ci-cd.svg" /><span>CI-CD</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/git.svg" /><span>Git</span></div>
-        <div class="flex gap-2 items-center "><img class="h-10 w-10"
-            src="./assets/imgs/skills/jira.svg" /><span>Jira</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/azure.svg" /><span>Azure</span></div>
-        <div class="flex gap-2 items-center"><img class="h-8 w-10" src="./assets/imgs/skills/aws.svg" /><span>AWS</span>
-        </div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/go.svg" /><span>GoLang</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/js.svg" /><span>JavaScript</span></div>
-        <div class="flex gap-2 items-center"><img class="h-10 w-10"
-            src="./assets/imgs/skills/typescript.svg" /><span>TypeScript</span></div>
-        <div class="flex gap-2 items-center "><img class="h-10 w-10"
-            src="./assets/imgs/skills/python.svg" /><span>Python</span></div>
-      </div>
-
-    </section>
+    
 
   </div>
 </template>
@@ -113,6 +27,8 @@ import CursorEffect from "./components/effects/CursorEffect.vue"
 import RotatingCircle from "./components/RotatingCircle.vue";
 import SpringHeading from "./components/SpringHeading.vue";
 import NavBar from "./components/NavBar.vue";
+import Skills from "./components/Skills.vue";
+
 
 
 document.addEventListener("scroll", function () {

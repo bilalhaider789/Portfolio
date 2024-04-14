8197793f1a1a9d5f221eb5ebe9f8d070b3050d72
bilalhaider789 <bilalhaider1311@gmail.com>
2024-04-14 16:53:10 +0500

    feat: added skills for frontend and backend

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
     <NavBar class="top-0 fixed z-50"></NavBar>
 
     <div class="">
-      <section class="lg:h-[100vh] pt-16 lg:pt-50 pb-24 flex flex-col lg:flex-row lg:items-center w-full lg:gap-16 bg-quaternary  ">
+      <section
+        class="lg:h-[100vh] pt-16 lg:pt-50 pb-24 flex flex-col lg:flex-row lg:items-center w-full lg:gap-16 bg-quaternary  ">
         <div class="flex w-full justify-center lg:w-1/2 lg:justify-end">
           <RotatingCircle></RotatingCircle>
         </div>
@@ -16,20 +17,65 @@
       </section>
     </div>
     <div class="custom-shape-divider-top-1708193282 bg-[#f8f8f8] mt-[-2px]">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            class="shape-fill fill-quaternary"></path>
-        </svg>
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path
+          d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+          class="shape-fill fill-quaternary"></path>
+      </svg>
+    </div>
+    <section class="bg-[#f8f8f8] min-h-[100vh] flex flex-col font-merienda px-4 lg:px-6">
+      <div class="w-full flex justify-center">
+        <h1 class="responsive-h1 lg:text-[3.5rem]">Skills</h1>
       </div>
-    <section class="bg-[#f8f8f8] min-h-[100vh] flex flex-col font-merienda">
-      <div class="w-full flex justify-center"><h1 class="responsive-h1 lg:text-[3.5rem]">Skills</h1></div>
-      <h2 class="responsive-h3 mt-4">FrontEnd</h2>
-      <div>
-        
+      <h2 class="responsive-h3 mt-4 font-medium">FrontEnd</h2>
+      <div class="skills responsive-p1 grid grid-cols-3 lg:grid-cols-4 gap-4 mt-4 lg:mt-6">
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/react.svg" /><span>React.js</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/vue.svg" /><span>Vue.js</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/nextjs.svg" /><span>Next.js</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/flutter.svg" /><span>Flutter</span></div>
+        <div class="flex gap-2 items-center "><img class="h-10 w-10"
+            src="./assets/imgs/skills/state-management.svg" /><span class="white">State Management</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/tailwind.svg" /><span>Tailwind Css</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/material-ui.svg" /><span>Material UI</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/html.svg" /><span>HTML</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/css.svg" /><span>CSS</span></div>
+      </div>
+      <h2 class="responsive-h3 mt-6 font-medium">BackEnd</h2>
+      <div class="skills responsive-p1 grid grid-cols-3 lg:grid-cols-4 gap-4 mt-4 lg:mt-6">
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/node.svg" /><span>Node</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/nestjs.svg" /><span>Nest</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/sequelize.svg" /><span>Sequelize</span></div>
+        <div class="flex gap-2 items-center "><img class="h-10 w-10"
+            src="./assets/imgs/skills/typeorm.svg" /><span>TypeOrm</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/database.svg" /><span>PostgresSQL</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/firebase.svg" /><span>FireBase</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/mongodb.svg" /><span>MongoDB</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10"
+            src="./assets/imgs/skills/influxdb.svg" /><span>InfluxDB</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10" src="./assets/imgs/skills/api.svg" /><span>Rest
+            Api</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10" src="./assets/imgs/skills/protobuf.svg" /><span>ProtoBuf</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10" src="./assets/imgs/skills/grpc.svg" /><span>gRPC</span></div>
+        <div class="flex gap-2 items-center"><img class="h-10 w-10" src="./assets/imgs/skills/flask.svg" /><span>Flask</span></div>
+        <div class="flex gap-2 items-center"><img class="h-8 w-10" src="./assets/imgs/skills/mqtt.svg" /><span>Mqtt</span></div>
+
       </div>
     </section>
-    
+
   </div>
 </template>
 
@@ -63,6 +109,18 @@ function isElementInViewport(el: any) {
 </script>
 
 <style scoped>
+
+
+.skills > div > img {
+  display: none;
+}
+
+@media (min-width: 768px) {
+  .skills > div > img {
+    display: block;
+  }
+}
+
 @keyframes fadeInText {
   from {
     opacity: 0;
